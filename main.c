11425ed2b2e2f9af1a5e8be4a269ee3711fd4a6a
LucasLima <lucas.lima716@etec.sp.gov.br>
2023-09-20 20:32:08 -0300

    primeiro commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("Boa noite!");
+	printf("Prova na proxima semana\n ");
+	printf("Corinthians, maior campeao paulista Subuxa nao tankou o Gojo!\n");
+	return 0;
+}
